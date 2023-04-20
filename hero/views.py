019b26d339a4f8b8aaaf49c2dc30ede973454242
VolskiyRoman <romanvolskiy791@gmail.com>
2023-04-20 11:28:43 +0300

    modified urls.py, templates/hero, index func

--- hero/views.py
+++ hero/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return HttpResponse("Hero app page.")
+    temp = Hero.objects.get(pk=1)
+    context = {
+       "name": temp.name,
+       "small": temp.image_url_small,
+       "large": temp.image_url_large
+    }
+    return render(request, "hero/index.html", context=context)
