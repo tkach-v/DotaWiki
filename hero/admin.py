04483ba9f6196bb45e8828cf1a538e8ae8697605
VolskiyRoman <romanvolskiy791@gmail.com>
2023-04-21 00:44:00 +0300

    updated database, rewrited settings.py, views.py, modified models

--- hero/admin.py
+++ hero/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Hero)
+admin.site.register(AttackType)
+admin.site.register(PrimaryStat)
--- hero/views.py
+++ hero/views.py
@@ -4,10 +4,4 @@ from .models import *
 
 
 def index(request):
-    temp = Hero.objects.get(pk=1)
-    context = {
-       "name": temp.name,
-       "small": temp.image_url_small,
-       "large": temp.image_url_large
-    }
-    return render(request, "hero/index.html", context=context)
+    return render(request, "hero/index.html")
