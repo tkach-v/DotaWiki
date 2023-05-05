70c828f2848573219c2a3cb4c2635ee6ed306b25
Volodymyr Tkach <vovantkac5@gmail.com>
2023-05-05 16:41:41 +0300

    Add ajax filter to hero_list

--- hero/views.py
+++ hero/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.views.generic import ListView
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import JsonResponse
+
 from .models import *
 
 
@@ -14,9 +15,24 @@ class HeroesList(ListView):
     context_object_name = 'heroes'
     queryset = Hero.objects.order_by('name')
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        complexity = self.request.GET.get('complexity')
-        if complexity:
-            queryset = queryset.filter(complexity=complexity)
-        return queryset
+    def dispatch(self, request, *args, **kwargs):
+        if 'X-Requested-With' in request.headers and request.headers['X-Requested-With'] == 'XMLHttpRequest':
+            if request.method == 'GET':
+                complexity = request.GET.get("complexity")
+                primary_ability = request.GET.get("primary_ability")
+
+                # Query the database based on the parameters
+                queryset = Hero.objects.order_by('name')
+                if complexity:
+                    queryset = queryset.filter(complexity=complexity)
+                if primary_ability:
+                    primary_stat = PrimaryStat.objects.get(name=primary_ability)
+                    queryset = queryset.filter(primary_stat=primary_stat)
+
+                # Serialize the queryset to JSON and return the response
+                heroes = list(queryset.values())
+                return JsonResponse({"context": heroes})
+
+            return JsonResponse({'status': 'Invalid request'}, status=400)
+        else:
+            return super().dispatch(request, *args, **kwargs)
